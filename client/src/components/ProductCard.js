5574625f55d77e17b06250d2c81de0f9f5ee5bb1
Osman Ali <okali@uci.edu>
2018-07-15 04:00:58 -0700

    Add SOLD OUT tag on product cards

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({ name, image, price }) => {
+const ProductCard = ({ name, image, price, stock }) => {
     return (
         <Link className="card-holder" to={`/products/${name}`} params={{ name, image, price }}>
             <div className="product-card">
                 <img src={`${image}`} alt={`${name}`} />
+                {(stock.small < 1 && stock.medium < 1 && stock.large < 1 && stock.xLarge < 1) &&
+                    <div className="overlay overlay--stock">
+                        Sold Out
+                    </div>
+                }
                 <div className="overlay">
                     <div>{name}</div>
                     <div>${price / 100}</div>
