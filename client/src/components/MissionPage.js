81a5b84b4c3bc325188c6fe5d008ea4f2e4ee5d2
Osman Ali <okali@uci.edu>
2018-07-19 19:42:17 -0700

    Restyle mission page

--- client/src/components/MissionPage.js
+++ client/src/components/MissionPage.js
@@ -7,13 +7,16 @@ const MissionPage = () => (
             We are all born equal, but we are not treated equally. This is why we must stand up for
             the equality of every person regardless of their race, religion, gender, and beliefs.
         </p>
-        <div className="goals-header"><span>M</span>ain goals</div>
+        {/*<div className="goals-header"><span>M</span>ain goals</div>*/}
+        <div style={{width: '100%', height: '2px', background: '#c6af40', marginBottom: '2rem' }}/>
         <div className="goals-container">
             <div className="goal">Show beauty in the small and simple things</div>
             <div className="goal">Dispel misconceptions about Muslims</div>
             <div className="goal">Create unity between communities</div>
             <div className="goal">Provide an outlet for people to express themselves in a fashionable way</div>
         </div>
+        <div style={{width: '100%', height: '2px', background: '#c6af40', marginBottom: '2rem' }}/>
+        <div className="mission-subtitle">Dont Be Afraid To Be Yourself</div>
     </div>
 );
 
